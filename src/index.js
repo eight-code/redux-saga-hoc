56d439bd67addc835faf9852dd7dd3f5c15857b8
Hajji Tarik <hajji-tarik@outlook.fr>
2017-03-10 00:33:18 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,45 +1,50 @@
 import React, { Component } from 'react';
 import { take, cancel, fork } from 'redux-saga/effects';
 
-export const sagaHOC = (WrappedComponent, sagas) => class SagaHOC extends Component {
-  constructor(props) {
-    super(props);
-    this.CANCEL_SAGAS = 'CANCEL_SAGAS';
-    this.constructor.displayName = `SagaHOC(${WrappedComponent.name})`;
-  }
-
-  static contextTypes = { store: React.PropTypes.object };
-
-  componentDidMount() {
-    this.startSagas();
-  }
-
-  createAbortableSaga = (saga) => {
-    if (process.env.NODE_ENV === 'development') {
-      return function* main() {
-        const sagaTask = yield fork(saga);
-
-        yield take(this.CANCEL_SAGAS);
-        yield cancel(sagaTask);
-      };
+export default function sagaHOC(WrappedComponent, sagas) {
+  return class SagaHOC extends Component {
+    constructor(props) {
+      super(props);
+      this.constructor.displayName = `SagaHOC(${WrappedComponent.name})`;
     }
-    return saga;
-  }
 
-  startSagas = () => {
-    sagas.map(this.createAbortableSaga).forEach(saga => this.context.store.runSaga(saga));
-  }
+    static contextTypes = { store: React.PropTypes.object };
 
-  cancelSagas() {
-    this.context.store.dispatch({
-      type: this.CANCEL_SAGAS,
-    });
-  }
+    componentDidMount() {
+      this.startSagas();
+    }
 
-  componentWillUnmount() {
-    this.cancelSagas();
-  }
+    createAbortableSaga = (saga) => {
+      if (process.env.NODE_ENV === 'development') {
+        return function* main() {
+          const sagaTask = yield fork(saga);
+          yield take('CANCEL_SAGAS');
+          yield cancel(sagaTask);
+        };
+      }
+      return saga;
+    }
 
-  render() {
-  }
-};
+    startSagas = () => {
+      sagas.map(this.createAbortableSaga).forEach(saga => this.context.store.runSaga(saga));
+    }
+
+    cancelSagas = () => {
+      this.context.store.dispatch({
+        type: 'CANCEL_SAGAS',
+      });
+    }
+
+    componentWillUnmount() {
+      this.cancelSagas();
+    }
+
+    render() {
+      return (<WrappedComponent
+        cancelSagas={this.cancelSagas}
+        runLocalSaga={this.createAbortableSaga}
+        {...this.props}
+      />);
+    }
+  };
+}
